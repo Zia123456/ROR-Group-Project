74bc4652589729f0dd3cf22a6b7fef909c4e9bc5
osamaashraf6 <osamaashraf668@gmail.com>
2023-03-17 14:55:48 +0200

    Add the recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,13 +1,61 @@
 class RecipesController < ApplicationController
-  def index; end
+  def index
+    @recipes = Recipe.all
+  end
 
-  def show; end
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
 
-  def new; end
+  def new
+    @recipe = Recipe.new
+  end
 
-  def create; end
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
 
-  def edit; end
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.author = current_user
+    # respond_to do |format|
+    #   if @recipe.save
+    #     format.html { redirect_to recipe_url(@recipe), notice: 'recipe made' }
+    #   else
+    #     format.html { render :new, alert: 'error of creating the recipe' }
+    #   end
+    # end
+    if @recipe.save
+      flash[:notice] = 'recipe created successfully!'
+      redirect_to recipes_path
+    else
+      flash[:alert] = "Couldn't create recipe!"
+      render :new, status: :unprocessable_entity
+    end
+  end
 
-  def destroy; end
+  def update
+    @recipe = Recipe.find(params[:id])
+    respond_to do |format|
+      if @recipe.update(recipe_params)
+        format.html { redirect_to recipe_url(@recipe), notice: 'recipe update' }
+      else
+        format.html { render :edit, alert: 'error of updating the recipe' }
+      end
+    end
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    respond_to do |format|
+      format.html { redirect_to recipes_url, notice: 'recipe deleted' }
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
+  end
 end

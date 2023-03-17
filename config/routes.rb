af00c84c930bcaeb390e4490628371a70459a8a4
osamaashraf6 <osamaashraf668@gmail.com>
2023-03-17 08:23:40 +0200

    Modify the routes of the recipe

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'foods#index'
-  resources :recipes, only: [:index]
+  resources :recipes, only: [:index, :new, :show, :edit]
   resources :inventories, only: [:index]
   resources :shopping_lists, only: [:index]
   resources :recipe_foods, only: [:index]

011f37172b526cb2091dd337f1a6d83d218c3571
yosefHesham <youssef.h@trufla.com>
2023-03-18 01:30:08 +0200

    Create inventory foods routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'foods#index'
+  delete 'inventory_foods/:id/destroy/:food_id', to: 'inventory_foods#destroy'
+  get 'inventory_foods/:id/new', to: 'inventory_foods#new'
+  post 'inventory_foods/:id/create', to: 'inventory_foods#create'
   devise_for :users
   resources :foods, only: %i[index new create destroy]
   resources :recipes, only: %i[index new create show update edit destroy]

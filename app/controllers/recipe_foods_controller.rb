f309c4f1f4449ee0111f887596dac64ec30477f5
Zia123456 <m.ziabakhteyari12@gmail.com>
2023-03-18 04:58:39 -0700

    Modify reoutes

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,8 +1,8 @@
 class RecipeFoodsController < ApplicationController
-  def destory; end
 
   def create
     recipe_foods = RecipeFood.new(quantity: params[:quantity])
+    puts "Creating new recipe"
     recipe_foods.food_id = params[:food_id]
     recipe_foods.recipe_id = params[:recipe_id]
     recipe_foods.save
@@ -10,25 +10,25 @@ class RecipeFoodsController < ApplicationController
     flash[:notice] = 'igredient added'
   end
 
-  def edit
-    @recipe = Recipe.find(params[:id])
-  end
+  # def edit
+  #   @recipe = Recipe.find(params[:id])
+  # end
 
-  def update
-    @recipe_food = RecipeFood.find(params[:id])
-    respond_to do |format|
-      if @recipe_food.update(recipe_food_params)
-        format.html { redirect_to recipe_url(@recipe_food), notice: 'recipe update' }
-      else
-        format.html { render :edit, alert: 'error of updating the recipe' }
-      end
-    end
-  end
+  # def update
+  #   @recipe_food = RecipeFood.find(params[:id])
+  #   respond_to do |format|
+  #     if @recipe_food.update(recipe_food_params)
+  #       format.html { redirect_to recipe_url(@recipe_food), notice: 'recipe update' }
+  #     else
+  #       format.html { render :edit, alert: 'error of updating the recipe' }
+  #     end
+  #   end
+  # end
 
   def destroy
     @recipe_food = RecipeFood.find(params[:id])
     @recipe_food.destroy
-    redirect_to recipe_path(Recipe.find(params[:recipe_id]))
+    redirect_to recipes_path, notice: "Recipe food was successfully deleted."
   end
 
   private

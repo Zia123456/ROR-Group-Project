25aabd19b4d1edcec1ebb7d60af37bfea00adc79
Zia123456 <m.ziabakhteyari12@gmail.com>
2023-03-18 05:26:46 -0700

    Modify Inventory model

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,2 +1,6 @@
 class Inventory < ApplicationRecord
+    validates :name, presence: true, length: { maximum: 100 }
+    validates :description, presence: true, length: { maximum: 200 }
+    belongs_to :user
+    has_many :inventory_foods
 end

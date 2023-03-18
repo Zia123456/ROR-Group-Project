e74e479e0bbe562b6ea2875bf7943a5c1bb39f1a
osamaashraf6 <osamaashraf668@gmail.com>
2023-03-18 22:40:22 +0200

    Fix the linter error

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+# rubocop:disable Metrics/BlockLength
+
 ActiveRecord::Schema[7.0].define(version: 20_230_318_120_156) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
@@ -93,3 +95,4 @@ ActiveRecord::Schema[7.0].define(version: 20_230_318_120_156) do
   add_foreign_key 'recipes', 'users'
   add_foreign_key 'recipes', 'users', column: 'author_id'
 end
+# rubocop:enable Metrics/BlockLength
